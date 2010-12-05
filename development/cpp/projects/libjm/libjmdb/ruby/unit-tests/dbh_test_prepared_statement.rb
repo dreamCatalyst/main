47fb80d414bf1d1c94db97bac0a5c753c384f432
Jonathan Maasland <dreamCatalyst@gmail.com>
2010-12-05 20:45:24 +0100

    Expanded the prepared_statement tests
    Added several new tests for prepared_statement and finished writing the
    remaining empty test stubs.
    
    Added a new field to the Item table so it now has a BigInt field.

--- development/cpp/projects/libjm/libjmdb/ruby/unit-tests/dbh_test_prepared_statement.rb
+++ development/cpp/projects/libjm/libjmdb/ruby/unit-tests/dbh_test_prepared_statement.rb
@@ -21,51 +21,116 @@ module TestPreparedStatement
   
   def test_pstatement_novars
     statement = prepare_query("select * from Customer")
-    result = statement.executeSelectQuery();
+    result = statement.executeSelectQuery
     check_select_result(result, 2)
   end
   
-  def test_bind_named_int_var
+  # ----------------- Simple bind tests
+  
+  def test_bind_int_by_name
     statement = prepare_query("select * from Customer where id=$id")
     statement.bind("$id", 1)
-    result = statement.executeSelectQuery();
+    result = statement.executeSelectQuery
     check_select_result(result, 1)
   end
   
-  def test_bind_named_int64
-    #assert(false, "test not written")
+  def test_bind_int_by_index
+    statement = prepare_query("select * from Customer where id=$id")
+    statement.bind(1, 1)
+    result = statement.executeSelectQuery
+    check_select_result(result, 1)
   end
   
-  def test_bind_named_double
-    #assert(false, "test not written")
+  def test_bind_int64_by_name
+    statement = prepare_query("select * from Item where retailId=$retailId")
+    statement.bind("$retailId", 1234567890123456789)
+    result = statement.executeSelectQuery
+    check_select_result(result, 1)
   end
   
-  def test_bind_named_string
-    #assert(false, "test not written")
+  def test_bind_int64_by_index
+    statement = prepare_query("select * from Item where retailId=$retailId")
+    statement.bind(1, 1234567890123456789)
+    result = statement.executeSelectQuery
+    check_select_result(result, 1)
   end
   
-  def test_bind_index_int
-    #assert(false, "test not written")
+  def test_bind_double_by_name
+    statement = prepare_query("select * from Item where price=$price")
+    statement.bind("$price", 18.95)
+    result = statement.executeSelectQuery
+    check_select_result(result, 2)
   end
   
-  def test_bind_indexed_int64
-    #assert(false, "test not written")
+  def test_bind_double_by_index
+    statement = prepare_query("select * from Item where price=$price")
+    statement.bind(1, 18.95)
+    result = statement.executeSelectQuery
+    check_select_result(result, 2)
   end
   
-  def test_bind_indexed_double
-    #assert(false, "test not written")
+  def test_bind_string_by_name
+    statement = prepare_query("select * from Customer where firstName=$name")
+    statement.bind("$name", "Jonathan")
+    result = statement.executeSelectQuery
+    check_select_result(result, 1)
   end
   
-  def test_bind_indexed_string
-    #assert(false, "test not written")
+  def test_bind_string_by_index
+    statement = prepare_query("select * from Customer where firstName=$name")
+    statement.bind(1, "Jonathan")
+    result = statement.executeSelectQuery
+    check_select_result(result, 1)
   end
   
-  def test_bind_multiple_variables
-    #assert(false, "test not written")
+  # ---------------- More complex bind tests
+  
+  def test_bind_multiple_variables_by_name
+    statement = prepare_query(
+      "select * from Customer where firstName=$fname and lastName=$lname")
+    statement.bind("$fname", "Jonathan")
+    statement.bind("$lname", "Maasland")
+    result = statement.executeSelectQuery
+    check_select_result(result, 1)
+  end
+  
+  def test_bind_multiple_variables_by_index
+    statement = prepare_query(
+      "select * from Customer where firstName=$fname and lastName=$lname")
+    statement.bind(1, "Jonathan")
+    statement.bind(2, "Maasland")
+    result = statement.executeSelectQuery
+    check_select_result(result, 1)
+  end
+  
+  def test_bind_multiple_variable_types_by_index
+    statement = prepare_query(
+      "select * from Item where price=$price and name like $name")
+    statement.bind(1, 18.95)
+    statement.bind(2, "%Weeds%")
+    result = statement.executeSelectQuery
+    check_select_result(result, 2)
+  end
+  
+  def test_bind_multiple_variable_types_by_name
+    statement = prepare_query(
+      "select * from Item where price=$price and name like $name")
+    statement.bind("$price", 18.95)
+    statement.bind("$name", "%Weeds%")
+    result = statement.executeSelectQuery
+    check_select_result(result, 2)
   end
   
   def test_multiple_executions
-    #assert(false, "test not written")
+    statement = prepare_query(
+      "select * from Customer where firstName like $fname")
+    statement.bind("$fname", "Jonathan")
+    result1 = statement.executeSelectQuery
+    check_select_result(result1, 1)
+    
+    statement.bind("$fname", "%")
+    result2 = statement.executeSelectQuery
+    check_select_result(result2, 2)
   end
   
   def test_sqlinjection
--- development/cpp/projects/libjm/libjmdb/ruby/unit-tests/fixtures/sqlite/sqlite_test_database.sql
+++ development/cpp/projects/libjm/libjmdb/ruby/unit-tests/fixtures/sqlite/sqlite_test_database.sql
@@ -17,7 +17,8 @@ create table Customer(
 create table Item(
   id INTEGER primary key,
   name varchar(255) not null,
-  price double not null
+  price double not null,
+  retailId bigint not null
 );
 
 create table PurchaseOrder(
@@ -39,11 +40,11 @@ insert into Customer (firstName, lastName, email, addresId) values ("Jonathan", 
 insert into Address (street,zipcode,city) values ("Some other street", "1234AC", "Someplace");
 insert into Customer (firstName, lastName, email, addresId) values ("A", "Nonymous", "", 2);
 
-insert into Item(name,price) values ("Weeds season 1 dvd-box", 18.95);
-insert into Item(name,price) values ("Weeds season 2 dvd-box", 18.95);
-insert into Item(name,price) values ("Weeds season 3 dvd-box", 28.95);
-insert into Item(name,price) values ("Weeds season 4 dvd-box", 38.95);
-insert into Item(name,price) values ("Weeds season 5 dvd-box", 38.95);
+insert into Item(name,price,retailId) values ("Weeds season 1 dvd-box", 18.95, 12345);
+insert into Item(name,price,retailId) values ("Weeds season 2 dvd-box", 18.95, 12346);
+insert into Item(name,price,retailId) values ("Weeds season 3 dvd-box", 28.95, 12347);
+insert into Item(name,price,retailId) values ("Weeds season 4 dvd-box", 38.95, 12348);
+insert into Item(name,price,retailId) values ("Weeds season 5 dvd-box", 38.95, 1234567890123456789);
 
 insert into PurchaseOrder(customerId,orderStatus) values(1,1);
 insert into PurchaseOrderLineItem(purchaseOrderId,itemId,quantity) values (1,1,1);
