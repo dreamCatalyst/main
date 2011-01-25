e72a892710a73e375ff1d729c8dc95368be2b53d
Jonathan Maasland <dreamCatalyst@gmail.com>
2011-01-25 22:02:35 +0100

    MovieMock - Added two methods to DirectoryHelper
    - added method get_user_home to reliably retrieve the users home
    	directory
    - added method mkdir_p which can be used to create directory structures
    	unlike Dir.mkdir which can only create one directory at a time.

--- development/ruby/projects/MovieMock/directory_helper.rb
+++ development/ruby/projects/MovieMock/directory_helper.rb
@@ -6,7 +6,7 @@
 class DirectoryHelper
   def self.files_filtered_by_extension(directory, extensions)
     re_string = ""
-    extensions.each do |ext| re_string += "(#{ext}$)|" end
+    extensions.each do |ext| re_string += "(\.#{ext}$)|" end
     re_string.chop!
     re = Regexp.new(re_string)
 
@@ -15,6 +15,34 @@ class DirectoryHelper
       @matched_files << filepath if filepath =~ re
     end
     return @matched_files
+  end
+
+  def self.get_user_home
+    home_var = [ "HOME", "HOMEPATH"].detect do |h| ENV[h] end
+    if home_var.nil?
+      puts "Could not detect your home directory! Please contact me (the author)"
+      return "."
+    end
+    return ENV[home_var]
+  end
+
+  # Creates the given path much like "mkdir -p" would do.
+  # TODO: How would we do this on Windows?
+  def self.mkdir_p(path)
+    existing_path = ""
+    path_components = path.split "/"
+
+    if path.start_with? "/"
+      path_components.shift
+    else
+      existing_path = path_components.shift
+    end
+    
+    path_components.each do |p|
+      existing_path += ("/" + p)
+      Dir.mkdir existing_path unless File.directory?(existing_path)
+    end
+    true
   end
 end
 
